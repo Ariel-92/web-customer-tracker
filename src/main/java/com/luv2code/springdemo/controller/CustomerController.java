9fcca553893389953157fe7c733b4093f8926b36
EH-Son <eunha.son08@gmail.com>
2022-11-01 11:35:31 +0900

    Update CustomerController.java
    - listcustomers() 메소드에서 Customer 데이터를 리스트로 전달하는 기능 추가

--- src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,30 @@
 package com.luv2code.springdemo.controller;
 
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/customer")
 public class CustomerController {
 
+    // need to inject the customer dao
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping(value = "/list")
     public String listCustomers(Model model) {
+        // get customer from te dao
+        List<Customer> customers = customerDAO.getCustomers();
+
+        // add the customers to the model
+        model.addAttribute("customers", customers);
+
         return  "list-customers";
     }
 
