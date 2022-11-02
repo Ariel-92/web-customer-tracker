577ac826258b2c0fbb449e27a99b06a698f244ad
EH-Son <eunha.son08@gmail.com>
2022-11-02 15:52:15 +0900

    design list-customer page and some update
    - add CSS files
    - add index page
    - refactoring CustomController
     - PostMapping to GetMapping

--- src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.luv2code.springdemo.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class CustomerController {
     @Autowired
     private CustomerDAO customerDAO;
 
-    @RequestMapping(value = "/list")
+    @GetMapping(value = "/list")
     public String listCustomers(Model model) {
         // get customer from te dao
         List<Customer> customers = customerDAO.getCustomers();
